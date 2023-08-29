08e2396dbd653c336eb6a4f8f1fe14e885b12876
Ícaro Chiabai <i247171@dac.unicamp.br>
2023-08-29 01:02:08 -0300

    feat: add selected property to tasks

--- todo-list/components/task/task.js
+++ todo-list/components/task/task.js
@@ -1,10 +1,8 @@
-
 import styles from './task.module.css'
 
 export default function Task(props) {
-  console.log(props)
   return (
-    <div className={styles.box} data-active={props.isChecked}>
+    <div className={styles.box} data-active={props.isSelected} onClick={props.onSelect}>
         <h2 className={styles.title}>{props.name}</h2>
         <p className={styles.description}>{props.description}</p>
     </div>
