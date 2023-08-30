cd5f044184b4ee478e27dfb49a2eb689fe273384
Ícaro Chiabai <i247171@dac.unicamp.br>
2023-08-30 03:40:41 -0300

    fix: improve task logic

--- todo-list/components/task/task.js
+++ todo-list/components/task/task.js
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import styles from './task.module.css'
+import { useIsEditing } from '@/app/context/TasksContext';
 
 export default function Task(props) {
-  return (
-    <div className={styles.box} data-active={props.isSelected} onClick={props.onSelect}>
-        <h2 className={styles.title}>{props.name}</h2>
-        <p className={styles.description}>{props.description}</p>
-    </div>
-  )
+  const { isEditing, setIsEditing } = useIsEditing();
+  if(props.isEditing) {
+        return(
+          <div className={styles.box} data-active={props.isSelected} onClick={props.onSelect}>
+            <input />
+            <p className={styles.description}>{props.description}</p>
+          </div>
+        )
+  } else {
+    return (
+      <div className={styles.box} data-active={props.isSelected} onClick={props.onSelect}>
+          <h2 className={styles.title}>{props.name}</h2>
+          <p className={styles.description}>{props.description}</p>
+      </div>
+    )
+  }
 }
