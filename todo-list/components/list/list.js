e1a7407d0fc81713e6244fbae638f435f697e6be
Ícaro Chiabai <i247171@dac.unicamp.br>
2023-08-30 03:39:58 -0300

    fix: improve list logic

--- todo-list/components/list/list.js
+++ todo-list/components/list/list.js
@@ -1,34 +1,33 @@
 "use client"
 import styles from './list.module.css'
 import Task from '../task/task'
-import { useState } from 'react'
+import { useContext, useState } from 'react'
+import { TasksContext, useSelectedTask } from '@/app/context/TasksContext'
 
 export default function List(props) {
-  const taskData = [
-    {id: 'task1', name: 'Task 1', description: 'Quick description about Task 1.', priority: '1', isChecked: '0'},
-    {id: 'task2', name: 'Task 2', description: 'Quick description about Task 2.', priority: '1', isChecked: '0'},
-    {id: 'task3', name: 'Task 3', description: 'Quick description about Task 3.', priority: '1', isChecked: '0'},
-    {id: 'task4', name: 'Task 4', description: 'Quick description about Task 4.', priority: '1', isChecked: '0'},
-    {id: 'task5', name: 'Task 5', description: 'Quick description about Task 5.', priority: '1', isChecked: '0'},
-    {id: 'task6', name: 'Task 6', description: 'Quick description about Task 6.', priority: '1', isChecked: '0'},
-    {id: 'task7', name: 'Task 7', description: 'Quick description about Task 7.', priority: '1', isChecked: '0'},
-    {id: 'task8', name: 'Task 8', description: 'Quick description about Task 8.', priority: '1', isChecked: '0'},
-  ]
+  const context = useContext(TasksContext);
+  const tasks = context['tasks'];
+  const { selectedTask, setSelectedTask } = useSelectedTask();
 
-  const [selectedTask, setSelectedTask] = useState(null)
-  const [tasks, setTasks] = useState(taskData)
-  const [taskCounter, setTaskCounter] = useState(tasks.length + 1)
-
-  const handleTaskSelect = (taskId) => { setSelectedTask(taskId) }
-  const handleTaskRemove = (taskId) => {
-    setTasks(tasks.filter(a => a.id !== taskId))
-  }
-  const handleTaskAdd = () => {
-    setTaskCounter(taskCounter + 1)
-    setTasks([...tasks, 
-        {id: 'task'+taskCounter, name: 'Task '+ taskCounter, description: 'Quick description about Task ' + taskCounter, priority: '1', isChecked: '0'}
-    ])
-  }
+  const handleTaskSelect = (taskId) => { 
+    if(selectedTask) {
+      if(tasks[selectedTask].id === taskId) {
+        setSelectedTask(null);
+      } else {
+        for(let i = 0; i < tasks.length; i++){
+          if(tasks[i].id === taskId) {
+            setSelectedTask(i);
+          }
+        }
+      }
+    } else {
+      for(let i = 0; i < tasks.length; i++){
+        if(tasks[i].id === taskId) {
+          setSelectedTask(i);
+        }
+      }
+    }
+  };
 
   return (
     <div className={styles.box}>
@@ -45,8 +44,9 @@ export default function List(props) {
                     description={task.description}
                     priority={task.priority}
                     isChecked={task.isChecked}
-                    isSelected={selectedTask === task.id}
+                    isSelected={selectedTask != null ? tasks[selectedTask].id === task.id : false}
                     onSelect={ () => handleTaskSelect(task.id) }
+                    isEditing ={task.isEditing}
                 />
             )
         )}
