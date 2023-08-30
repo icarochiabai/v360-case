5b8b8c49185b9e009688e47b2dc5dc1d697d4a89
Ícaro Chiabai <i247171@dac.unicamp.br>
2023-08-30 03:40:13 -0300

    fix: improve menu logic

--- todo-list/components/menu/menu.js
+++ todo-list/components/menu/menu.js
@@ -1,14 +1,46 @@
+import { SelectedTask, TasksContext, TasksDispatchContext, TasksProvider, useIdCounter, useIsEditing, useSelectedTask } from '@/app/context/TasksContext'
 import styles from './menu.module.css'
+import { useContext } from 'react'
+import { FormContext, useDescription, useIsVisible, usePriority, useTitle } from '../form/FormContext';
 
 export default function Menu(props) {
     if(props.menuType === "task") {
+        const dispatch = useContext(TasksDispatchContext);
+        const { isVisible, setIsVisible } = useIsVisible();
+        const { selectedTask, setSelectedTask } = useSelectedTask();
+        const { isEditing, setIsEditing } = useIsEditing();
+        const tasks = useContext(TasksContext)["tasks"];
+        const { title, setTitle } = useTitle();
+        const { description, setDescription } = useDescription();
+        const { priority, setPriority } = usePriority();
+
         return (
-            <div className={styles.taskMenu}>
-                <button>+</button>
-                <button>%</button>
-                <button>-</button>
-                <button>x</button>
-            </div>
+                <div className={styles.taskMenu}>
+                    <button onClick={ () => {
+                        setTitle('');
+                        setDescription('');
+                        setPriority(0);
+                        setSelectedTask(null);
+                        setIsVisible(!isVisible);
+                    }
+                    }>+</button>
+                    <button onClick={ () => {
+                        if(selectedTask != null) {
+                            setTitle(tasks[selectedTask].name);
+                            setDescription(tasks[selectedTask].description);
+                            setPriority(tasks[selectedTask].priority);
+                            setIsVisible(!isVisible);
+                        }
+                    }}>%</button>
+                    <button onClick={ () => {
+                        dispatch({
+                        type:'deleted',
+                        id: selectedTask,
+                        });
+                    }
+                    }>-</button>
+                    <button>x</button>
+                </div>
         )
     } else if (props.menuType === "list") {
         return (
