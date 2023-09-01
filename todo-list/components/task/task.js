a3c0af41db0b65b5edb1d451b292e1f85a09523e
Ícaro Chiabai <i247171@dac.unicamp.br>
2023-09-01 05:11:58 -0300

    format: add description indicator

--- todo-list/components/task/task.js
+++ todo-list/components/task/task.js
@@ -5,8 +5,11 @@ import { useIsEditing } from '@/context/TasksContext';
 export default function Task(props) {
   return (
     <div className={styles.box} data-active={props.isSelected} onClick={props.onSelect}>
+      <div className={styles.top}>
         {props.isChecked ? <s>asdasd<h2 className={styles.title}>{props.name}</h2></s> : <h2 className={styles.title}>{props.name}</h2>}
-        <p className={styles.description}>{props.description}</p>
+        {props.description != "" ? <i class="fa-solid fa-circle-info"></i> : "" }
+      </div>
+      <p className={styles.description}>{props.description}</p>
     </div>
   )
 }
