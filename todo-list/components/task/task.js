61ae94041a7b033c616bf4dc6ba00503d286b7c9
Ícaro Chiabai <i247171@dac.unicamp.br>
2023-09-01 02:43:48 -0300

    refactor: add conditional render to tasks

--- todo-list/components/task/task.js
+++ todo-list/components/task/task.js
@@ -1,22 +1,12 @@
 import { useState } from 'react';
 import styles from './task.module.css'
-import { useIsEditing } from '@/app/context/TasksContext';
+import { useIsEditing } from '@/context/TasksContext';
 
 export default function Task(props) {
-  const { isEditing, setIsEditing } = useIsEditing();
-  if(props.isEditing) {
-        return(
-          <div className={styles.box} data-active={props.isSelected} onClick={props.onSelect}>
-            <input />
-            <p className={styles.description}>{props.description}</p>
-          </div>
-        )
-  } else {
-    return (
-      <div className={styles.box} data-active={props.isSelected} onClick={props.onSelect}>
-          <h2 className={styles.title}>{props.name}</h2>
-          <p className={styles.description}>{props.description}</p>
-      </div>
-    )
-  }
+  return (
+    <div className={styles.box} data-active={props.isSelected} onClick={props.onSelect}>
+        {props.isChecked ? <s>asdasd<h2 className={styles.title}>{props.name}</h2></s> : <h2 className={styles.title}>{props.name}</h2>}
+        <p className={styles.description}>{props.description}</p>
+    </div>
+  )
 }
