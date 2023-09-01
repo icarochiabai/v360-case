b177bfa0cdcba06ae3ca907dc180e00abc679812
Ícaro Chiabai <i247171@dac.unicamp.br>
2023-09-01 02:41:23 -0300

    feat: add mode to form

--- todo-list/components/form/form.js
+++ todo-list/components/form/form.js
@@ -1,7 +1,7 @@
 import { useContext, useState } from 'react';
 import styles from './form.module.css'
-import { FormContext, useDescription, useIsVisible, usePriority, useTitle } from './FormContext';
-import { TasksContext, TasksDispatchContext, useIdCounter, useSelectedTask } from '@/app/context/TasksContext';
+import { FormContext, useDescription, useIsVisible, useMode, usePriority, useTitle } from '../../context/FormContext';
+import { TasksContext, TasksDispatchContext, useIdCounter, useSelectedTask } from '@/context/TasksContext';
 import Task from '../task/task';
 
 export default function Form(props) {
@@ -13,6 +13,7 @@ export default function Form(props) {
     const { description, setDescription } = useDescription();
     const { priority, setPriority } = usePriority();
     const { selectedTask, setSelectedTask } = useSelectedTask();
+    const { mode, setMode } = useMode();
     const tasks = useContext(TasksContext)["tasks"];
 
     const { idCounter, setIdCounter } = useIdCounter();
@@ -22,6 +23,7 @@ export default function Form(props) {
         setDescription('');
         setPriority(0);
         setSelectedTask(null);
+        setMode('');
     }
 
     const handleAddTask = () => {
@@ -45,7 +47,7 @@ export default function Form(props) {
                     name: title,
                     description: description,
                     priority: priority,
-                    isChecked: 0,
+                    isChecked: false,
                     isEditing: false,
                 });
 
@@ -59,21 +61,20 @@ export default function Form(props) {
     return (
         <>
         <form className={styles.form} data-active={isVisible}>
-            <label>Title</label>
-            <input type="text" placeholder="Enter title" name="title" value={title} onChange={e => setTitle(e.target.value)}/>
-            
-            <label>Description</label>
-            <input type="text" placeholder="Enter description" name="description" value={description} onChange={e => setDescription(e.target.value)}/>
-            
-            <label>Priority</label>
-            <input type="number" placeholder="Enter priority" min="0" name="priority" value={priority} onChange={e => setPriority(e.target.value)}/>
-
-            <button
-                type="button"
-                onClick= { handleAddTask }>
-                Confirmar
-            </button>
-
+            <h1>{mode == 'adding' && mode != '' ? "Create task" : "Modify task" }</h1>
+            <div className={styles.row1}>
+                <input type="text" placeholder="Write something to do here..." name="title" value={title} onChange={e => setTitle(e.target.value)}/>
+            </div>
+            <div className={styles.row2}>
+                <input type="text" placeholder="Description (optional)" name="description" value={description} onChange={e => setDescription(e.target.value)}/>
+                <input type="number" placeholder="Enter priority" min="0" name="priority" value={priority} onChange={e => setPriority(e.target.value)}/>
+                <button
+                    type="button"
+                    onClick= { handleAddTask }
+                    >
+                    {mode == 'adding' && mode != '' ? <i className="fa-solid fa-plus"></i> : <i className="fa-solid fa-pencil"></i>}
+                </button>
+            </div>
         </form>
         </>
 
