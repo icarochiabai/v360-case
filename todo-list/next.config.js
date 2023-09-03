daa6b39990a9479818d37f87feae8db01c077117
Ícaro Chiabai <i247171@dac.unicamp.br>
2023-09-03 20:09:12 -0300

    build: add file loader to handle mp3

--- todo-list/next.config.js
+++ todo-list/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  webpack: (config, options) => {
+    config.module.rules.push(      {
+      test: /\.(jpe?g|png|svg|gif|ico|eot|ttf|woff|woff2|mp4|pdf|webm|mp3|wav)$/,
+      type: 'asset',
+      generator: {
+        filename: 'static/chunks/[path][name].[hash][ext]'
+      },
+    });
+
+      return config;
+  }
+};
