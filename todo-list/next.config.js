df7bf83f2344c5c8d6869252c095a191ee4a81bc
Ícaro Chiabai <i247171@dac.unicamp.br>
2023-09-03 21:12:17 -0300

    fix: add export output to next config

--- todo-list/next.config.js
+++ todo-list/next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  webpack: (config, options) => {
-    config.module.rules.push(      {
-      test: /\.(jpe?g|png|svg|gif|ico|eot|ttf|woff|woff2|mp4|pdf|webm|mp3|wav)$/,
-      type: 'asset',
-      generator: {
-        filename: 'static/chunks/[path][name].[hash][ext]'
-      },
-    });
-
-      return config;
-  }
+    output: 'export',
+    webpack: (config, options) => {
+        config.module.rules.push(      {
+        test: /\.(jpe?g|png|svg|gif|ico|eot|ttf|woff|woff2|mp4|pdf|webm|mp3|wav)$/,
+        type: 'asset',
+        generator: {
+            filename: 'static/chunks/[path][name].[hash][ext]'
+        },
+        });
+        return config;
+    }
 };
